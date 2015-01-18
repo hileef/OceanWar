4a1094adc6b11f1332681c3f37d41a23fdd7332f
killwithabass <acolonna@etu.utc.fr>
2015-01-18 17:36:09 +0100

    Debut java doc
    case et bateau

--- src/modele/Bateau.java
+++ src/modele/Bateau.java
@@ -1,29 +1,71 @@
 package modele;
 
 import java.util.LinkedList;
-
+/**
+ * Contient les attributs et les méthodes permettant de gérer l'objet BATEAU.
+ */
 public abstract class Bateau {
-	
+	/**
+	 * Définit à combien de tirs peuvent résister les bateaux.
+	 */
 	private static final int resistanceMax = 5;
 	
+	/**
+	 * Définit le nombre de "vies" qu'a le bateau.
+	 */
 	private int vies;
+	
+	/**
+	 * Attribut de typeCase contenant la case ou le bateau se trouve.
+	 */
 	private Case position;
+	
+	/**
+	 * Attribut de type DIRECTION qui permet au bateau de connaître sa direction de déplacement.
+	 */
 	private DIRECTION direction;
 	
+	/**
+	 * Définit l'action du bateau.
+	 */
 	public abstract void agir() ;
 	
+	/**
+	 * Permet de récupérer des informations sur l'entourage du bateau et ainsi changer certains attributs de BATEAU.
+	 * @param bateauLePlusProche
+	 * @param dir
+	 * @param distance
+	 */
 	public abstract void infoEntourage(Case bateauLePlusProche, DIRECTION dir, int distance) ;
 	
+	/**
+	 * Permet de mettre à jour la position du bateau
+	 * @param destination Case dans laquelle il va être déplacé.
+	 */
 	public void position(Case destination) {
 		this.position = destination;
 	}
+	
+	/**
+	 * Permet de connaître la case dans laquelle se trouve un bateau.
+	 * @return la case dans laquelle est le bateau.
+	 */
 	public Case position() {
 		return position;
 	}
 	
+	/**
+	 * Getteur de direction
+	 * @return la direction dans laquelle se déplace le bateau.
+	 */
 	protected DIRECTION direction() {
 		return direction;
 	}
+	
+	/**
+	 * Setter de direction
+	 * @param d une direction
+	 */
 	protected void direction(DIRECTION d) {
 		direction = d;
 	}
@@ -40,12 +82,24 @@ public abstract class Bateau {
 	
 	public abstract DIRECTION determinerDirection() ;
 	
+	/**
+	 * supprime une vie au bateau lorsque la méthode est appelée.
+	 */
 	public void toucher() { 
 		vies--;
 	}
+	
+	/**
+	 * Remonte le niveau de vie à son maximum.
+	 */
 	public void reparer() { 
 		vies = resistanceMax;
 	}
+	
+	/**
+	 * Getteur de vies.
+	 * @return le nombre de vies qu'a le bateau.
+	 */
 	public int vies() {
 		return vies;
 	}
