3c75b23131c5cad2164e6de6493a0ca1dc7fe7f7
killwithabass <acolonna@etu.utc.fr>
2015-01-26 12:05:55 +0100

    Update Bateau.java

--- src/modele/Bateau.java
+++ src/modele/Bateau.java
@@ -5,16 +5,16 @@ import java.util.LinkedList;
 import affichage.Affichable;
 import controle.Simulation;
 /**
- * Contient les attributs et les m�thodes permettant de g�rer l'objet BATEAU.
+ * Contient les attributs et les mï¿½thodes permettant de gï¿½rer l'objet BATEAU.
  */
 public abstract class Bateau implements Affichable {
 	/**
-	 * D�finit � combien de tirs peuvent r�sister les bateaux.
+	 * Dï¿½finit ï¿½ combien de tirs peuvent rï¿½sister les bateaux.
 	 */
 	private static final int resistanceMax = 5;
 	
 	/**
-	 * D�finit le nombre de "vies" qu'a le bateau.
+	 * Dï¿½finit le nombre de "vies" qu'a le bateau.
 	 */
 	private int vies;
 	
@@ -40,15 +40,15 @@ public abstract class Bateau implements Affichable {
 	}
 	
 	/**
-	 * Permet de mettre � jour la position du bateau
-	 * @param destination Case dans laquelle il va �tre d�plac�.
+	 * Permet de mettre ï¿½ jour la position du bateau
+	 * @param destination Case dans laquelle il va ï¿½tre dï¿½placï¿½.
 	 */
 	public void position(Coordonee destination) {
 		this.position = destination;
 	}
 	
 	/**
-	 * Permet de conna�tre la case dans laquelle se trouve un bateau.
+	 * Permet de connaï¿½tre la case dans laquelle se trouve un bateau.
 	 * @return la case dans laquelle est le bateau.
 	 */
 	public Coordonee position() {
@@ -57,14 +57,11 @@ public abstract class Bateau implements Affichable {
 	
 	
 	/**
-	 * Attribut de type DIRECTION qui permet au bateau de conna�tre sa direction de d�placement.
+	 * Attribut de type DIRECTION qui permet au bateau de connaï¿½tre sa direction de dï¿½placement.
 	 */
 	private DIRECTION direction;
 	
-	/**
-	 * D�finit l'action du bateau.
-	 */
-	public abstract void agir() ;
+	
 	
 	// TODO DOC A REFAIRE
 	public abstract void infosRadar(LinkedList<Bateau> listeMemeEndroit, LinkedList<Bateau> listeTrieeEntourage) ;
@@ -73,7 +70,7 @@ public abstract class Bateau implements Affichable {
 	
 	/**
 	 * Getteur de direction
-	 * @return la direction dans laquelle se d�place le bateau.
+	 * @return la direction dans laquelle se dï¿½place le bateau.
 	 */
 	protected DIRECTION direction() {
 		return direction;
@@ -86,78 +83,20 @@ public abstract class Bateau implements Affichable {
 	protected void direction(DIRECTION d) {
 		direction = d;
 	}
-	protected DIRECTION directionAleatoire() {
-		LinkedList<DIRECTION> options = new LinkedList<DIRECTION>();
-		if(!position().estUneBordure(0, Ocean.TAILLE_MATRICE - 1))
-			return directionAleatoireSansPenserAuBords();
-		switch(position().interpreterDirectionBordure(0, Ocean.TAILLE_MATRICE - 1)) {
-		case NO :
-			options.add(DIRECTION.SE);
-			options.add(DIRECTION.S);
-			options.add(DIRECTION.E);
-			break;
-		case NE :
-			options.add(DIRECTION.SO);
-			options.add(DIRECTION.S);
-			options.add(DIRECTION.O);
-			break;
-		case SO :
-			options.add(DIRECTION.NE);
-			options.add(DIRECTION.N);
-			options.add(DIRECTION.E);
-			break;
-		case SE :
-			options.add(DIRECTION.NO);
-			options.add(DIRECTION.N);
-			options.add(DIRECTION.O);
-			break;
-		case N :
-			options.add(DIRECTION.S);
-			options.add(DIRECTION.SE);
-			options.add(DIRECTION.SO);
-			break;
-		case S :
-			options.add(DIRECTION.N);
-			options.add(DIRECTION.NE);
-			options.add(DIRECTION.NO);
-			break;
-		case O :
-			options.add(DIRECTION.E);
-			options.add(DIRECTION.NE);
-			options.add(DIRECTION.SE);
-			break;
-		case E :
-			options.add(DIRECTION.O);
-			options.add(DIRECTION.NO);
-			options.add(DIRECTION.SO);
-			break;
-		default:
-			return directionAleatoireSansPenserAuBords();
-		}
-		return directionAleatoireDepuis(options);
-	}
-	protected DIRECTION directionAleatoireSansPenserAuBords() {
-		LinkedList<DIRECTION> liste = new LinkedList<DIRECTION>();
-		for(DIRECTION d : DIRECTION.values())
-			liste.add(d);
-		return directionAleatoireDepuis(liste);
-	}
-	protected DIRECTION directionAleatoireDepuis(LinkedList<DIRECTION> options) {
-		return options.get((int) (Math.random() * (options.size() - 1)));
-	}
+	
 	
 	public abstract DIRECTION determinerDirection() ;
 	
 	/**
-	 * supprime une vie au bateau lorsque la m�thode est appel�e.
+	 * supprime une vie au bateau lorsque la mï¿½thode est appelï¿½e.
 	 */
 	public void toucher() { 
 		vies--;
-		System.out.println("DEBUG : " + this.toString() + "#" + this.id() + " à été touché. vies = " + vies);
+		System.out.println("DEBUG : " + this.toString() + "#" + this.id() + " Ã  Ã©tÃ© touchÃ©. vies = " + vies);
 	}
 	
 	/**
-	 * Remonte le niveau de vie � son maximum.
+	 * Remonte le niveau de vie ï¿½ son maximum.
 	 */
 	public void reparer() { 
 		vies = resistanceMax;
